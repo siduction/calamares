af7b05ec8c47b292b5e21961d8d1e80138850fcc
Alf Gaida <agaida@siduction.org>
2019-02-21 23:30:01 +0100

    New snapshot: 3.2.4v3.2.4-1

--- ci/travis-continuous.sh
+++ ci/travis-continuous.sh
@@ -12,25 +12,44 @@ test -f $SRCDIR/CMakeLists.txt || { echo "! Missing $SRCDIR/CMakeLists.txt" ; ex
 
 cd $BUILDDIR || exit 1
 
+section() {
 echo "###"
-echo "### cmake $CMAKE_ARGS $SRCDIR"
+echo "### $1"
 echo "###"
+pwd -P
+df -h
+}
+
+section "cmake $CMAKE_ARGS $SRCDIR"
 cmake $CMAKE_ARGS $SRCDIR || { echo "! CMake failed" ; exit 1 ; }
 
-echo -e "###\n### make\n###"
-make -j2 || { make -j1 VERBOSE=1 ; echo "! Make failed" ; exit 1 ; }
+section "make"
+make -j2 || { echo "! Make recheck" ; pwd -P ; df -h ; make -j1 VERBOSE=1 ; echo "! Make failed" ; exit 1 ; }
 
-echo -e "###\n### make install\n###"
+section "make install"
+
+install_debugging() {
+	ls -la $( find "$1" -type f -name '*.so' )
+}
+
 echo "# Build results"
-find "$BUILDDIR" -name '*.so'
+install_debugging "$BUILDDIR"
 
 echo "# Install"
 DESTDIR=/build/INSTALL_ROOT
 mkdir -p "$DESTDIR"
 
-result=true
-make install VERBOSE=1 DESTDIR="$DESTDIR" || result=false
+if make install VERBOSE=1 DESTDIR="$DESTDIR" ;
+then
+	echo "# .. install OK"
+	result=true
+else
+	echo "# .. install failed"
+	result=false
+fi
 
-echo "# Install results"
-find "$DESTDIR" -type f
-$result  # Result of make install, above
+
+section "Install results"
+install_debugging "$DESTDIR"
+
+$result || { echo "! Install failed" ; exit 1 ; } # Result of make install, above
--- debian/repository.spec
+++ debian/repository.spec
@@ -15,12 +15,12 @@
 # =========
 # cleanup ()
 
-baseversion=3.2.2
+baseversion=3.2.4
 distribution=unstable
 uploadrepo=extra
 
 repository="https://github.com/calamares/calamares"
-branch='-b v3.2.2 --single-branch --depth=1'
+branch='-b v3.2.4 --single-branch --depth=1'
 build="yes"
 clean="yes"
 gitcommit="yes"
